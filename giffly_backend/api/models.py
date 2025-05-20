3bd6b75b58ce31f384a2434479cd0ffd0ede26a4
danielsteblev <groupnice@bk.ru>
2025-05-20 22:38:07 +0300

    fix

--- giffly_backend/api/models.py
+++ giffly_backend/api/models.py
@@ -40,13 +40,13 @@ class User(AbstractUser):
 
 # товар
 class Product(models.Model):
+    seller = models.ForeignKey('User', on_delete=models.CASCADE, related_name='products')
     name = models.CharField(max_length=255)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
     image_url = models.ImageField(upload_to='products/', null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    seller = models.ForeignKey('User', on_delete=models.CASCADE, related_name='products')
 
     def __str__(self):
         return self.name

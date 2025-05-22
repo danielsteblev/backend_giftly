7c1973a12a76df39ce9d9ad49876af866965fb75
danielsteblev <groupnice@bk.ru>
2025-05-22 20:04:05 +0300

    add: миграция для регистрации

---
+++ giffly_backend/api/migrations/0004_merge_20250522_1700.py
@@ -0,0 +1,10 @@
+from django.db import migrations
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ('api', '0003_fix_authtoken'),
+        ('api', '0003_merge_20250522_1659'),
+    ]
+
+    operations = [
+    ] 
